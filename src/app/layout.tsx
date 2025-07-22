b951345de2926a7aba414ca33d500a9bf4a70cba
RTN  GLOBAL <muhammadnouman72321@gmail.com>
2025-07-23 06:40:13 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,6 +118,17 @@ export default function RootLayout({
         <WebVitals />
         <GoogleAnalytics />
         <GoogleTagManager />
+        {/* Trustpilot Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
+                  a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
+                  f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+                  tp('register', 'x6BksEp48x1wgNc2');
+            `
+          }}
+        />
         {/* TrustBox script */}
         <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
         {/* End TrustBox script */}
